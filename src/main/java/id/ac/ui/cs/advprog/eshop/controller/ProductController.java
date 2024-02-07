ae2db658b2af6323cfa3180ff86c059c0154aa3f
adipppp <adhif2004@gmail.com>
2024-02-07 18:42:33 +0700

    Add controller for deleteProduct view & make createProduct view display error messages

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
 
 import java.util.List;
 
@@ -16,23 +19,77 @@ public class ProductController {
     @Autowired
     private ProductService service;
 
-    @GetMapping("/create")
+    @GetMapping({"/create", "/create/"})
     public String createProductPage(Model model) {
         Product product = new Product();
         model.addAttribute("product", product);
         return "createProduct";
     }
 
-    @PostMapping("/create")
-    public String createProductPost(Model model, @ModelAttribute Product product) {
-        service.create(product);
-        return "redirect:list";
+    @PostMapping({"/create", "/create/"})
+    public String createProductPost(Model model, @ModelAttribute Product product, BindingResult result) {
+        try {
+            if (result.hasErrors())
+                throw new RuntimeException("Product quantity is not an integer");
+            service.create(product);
+        } catch (RuntimeException exception) {
+            exception.printStackTrace();
+
+            String messageToDisplay;
+            String exceptionMessage = exception.getMessage();
+            switch (exceptionMessage) {
+                case "Field Product.productQuantity is less than 0":
+                    messageToDisplay = "Product quantity cannot be negative";
+                    break;
+                case "Product quantity is not an integer":
+                    messageToDisplay = "Product quantity is not an integer";
+                    break;
+                case "Field Product.productName has 0 length":
+                    messageToDisplay = "Product name should not be left empty";
+                    break;
+                case "Field Product.productName is null":
+                    messageToDisplay = "Request body is invalid";
+                    break;
+                default:
+                    messageToDisplay = "An unknown exception has occured";
+            }
+
+            model.addAttribute("error", messageToDisplay);
+            return "createProduct";
+        }
+
+        return "redirect:/product/list";
     }
 
-    @GetMapping("/list")
+    @GetMapping({"/list", "/list/"})
     public String productListPage(Model model) {
         List<Product> allProducts = service.findAll();
         model.addAttribute("products", allProducts);
         return "productList";
+    }
+
+    @GetMapping({"/delete", "/delete/", "/delete/{productId}", "/delete/{productId}"})
+    public String deleteProductPage(Model model, @PathVariable(required=false) String productId) {
+        Product product;
+        try {
+            product = service.findOne(productId);
+        } catch (RuntimeException exception) {
+            exception.printStackTrace();
+            return "redirect:/product/list";
+        }
+        model.addAttribute("product", product);
+        return "deleteProduct";
+    }
+
+    @PostMapping({"/delete", "/delete/", "/delete/{productId}", "/delete/{productId}"})
+    public String deleteProductPost(Model model, @PathVariable(required=false) String productId, @ModelAttribute Product product, BindingResult result) {
+        product.setProductId(productId);
+        try {
+            service.delete(product);
+        } catch (RuntimeException exception) {
+            exception.printStackTrace();
+        }
+
+        return "redirect:/product/list";
     }
 }
