5232a2e3fd13878f8301ea6bf702cd375f3edb31
adipppp <adhif2004@gmail.com>
2024-02-14 19:03:35 +0700

    Update workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
 	jacoco
 	id("org.springframework.boot") version "3.2.2"
 	id("io.spring.dependency-management") version "1.1.4"
+	id("org.sonarqube") version "4.4.1.3373"
 }
 
 group = "id.ac.ui.cs.advprog"
@@ -15,6 +16,14 @@ val junitJupiterVersion = "5.9.1"
 
 java {
 	sourceCompatibility = JavaVersion.VERSION_21
+}
+
+sonar {
+  properties {
+    property("sonar.projectKey", "adipppp_tutorial-advprog")
+    property("sonar.organization", "adipppp")
+    property("sonar.host.url", "https://sonarcloud.io")
+  }
 }
 
 configurations {
