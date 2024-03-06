794f31cf8052fb685d5393f1930d31de4d87ecbc
adipppp <adhif2004@gmail.com>
2024-03-06 20:28:53 +0700

    [GREEN] Implement PaymentRepository

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
@@ -1,6 +1,8 @@
 package id.ac.ui.cs.advprog.eshop.repository;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import org.springframework.stereotype.Repository;
 
@@ -8,17 +10,33 @@ import id.ac.ui.cs.advprog.eshop.model.Payment;
 
 @Repository
 public class PaymentRepository {
-    private List<Payment> paymentData;
+    private List<Payment> paymentData = new ArrayList<>();
 
     public Payment save(Payment payment) {
-        return null;
+        int i = 0;
+        for (Payment savedPayment : paymentData) {
+            if (savedPayment.getId().equals(payment.getId())) {
+                paymentData.remove(i);
+                paymentData.add(i, payment);
+                return payment;
+            }
+            i += 1;
+        }
+
+        paymentData.add(payment);
+        return payment;
     }
 
     public Payment findById(String id) {
-        return null;
+        for (Payment savedPayment : paymentData) {
+            if (savedPayment.getId().equals(id)) {
+                return savedPayment;
+            }
+        }
+        throw new NoSuchElementException();
     }
 
     public List<Payment> findAll() {
-        return null;
+        return paymentData;
     }
 }
