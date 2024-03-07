62b50c3949c3955d4b662de4669b296cadea5692
adipppp <adhif2004@gmail.com>
2024-03-07 14:10:46 +0700

    [RED] Add InvalidVoucherCodeException skeleton

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/exceptions/payment/InvalidVoucherCodeException.java
@@ -0,0 +1,15 @@
+package id.ac.ui.cs.advprog.eshop.exceptions.payment;
+
+public class InvalidVoucherCodeException extends RuntimeException {
+    public InvalidVoucherCodeException() {
+    }
+
+    public InvalidVoucherCodeException(String message) {
+    }
+
+    public InvalidVoucherCodeException(Throwable cause) {
+    }
+
+    public InvalidVoucherCodeException(String message, Throwable cause) {
+    }
+}
